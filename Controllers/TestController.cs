04efea7d86a30d95fc48de6a459bf4e04e852a1c
Dan G <75383639+ddgroleau@users.noreply.github.com>
2021-04-27 17:08:57 -0600

    removed unneccessary auto-generated items. Built basic get functionality

---
+++ Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<int>> Get()
+        {
+            return new List<int> { 1, 2, 3, 4 };
+        }
+    }
+}
